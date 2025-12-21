7a41c0f3155c28895329f18464ba56fc9aec205e
KusouUK <orion26br@gmail.com>
2025-12-21 15:55:07 -0300

    feat: get events with pagination

--- src/main/java/com/vitoruk/api/controller/EventController.java
+++ src/main/java/com/vitoruk/api/controller/EventController.java
@@ -2,11 +2,14 @@ package com.vitoruk.api.controller;
 
 import com.vitoruk.api.domain.event.Event;
 import com.vitoruk.api.domain.event.EventRequestDTO;
+import com.vitoruk.api.domain.event.EventResponseDTO;
 import com.vitoruk.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -14,7 +17,6 @@ public class EventController {
 
     @Autowired
     private EventService eventService;
-    private Event newEvent;
 
     @PostMapping(consumes = "multipart/form-data")
     public ResponseEntity<Event> create(@RequestParam("title") String title,
@@ -26,5 +28,12 @@ public class EventController {
         EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, state, remote, eventUrl, image);
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page,
+                                                            @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDTO> allEvents = this.eventService.getEvents(page, size);
+        return ResponseEntity.ok(allEvents);
     }
 }
--- src/main/java/com/vitoruk/api/service/EventService.java
+++ src/main/java/com/vitoruk/api/service/EventService.java
@@ -2,9 +2,13 @@ package com.vitoruk.api.service;
 
 import com.vitoruk.api.domain.event.Event;
 import com.vitoruk.api.domain.event.EventRequestDTO;
+import com.vitoruk.api.domain.event.EventResponseDTO;
 import com.vitoruk.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -15,6 +19,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -46,6 +51,16 @@ public class EventService {
 
         eventRepository.save(newEvent);
         return newEvent;
+    }
+
+    public List<EventResponseDTO> getEvents(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.eventRepository.findAll(pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(),
+                        event.getDescription(), event.getDate(), "", "", event.getRemote(),
+                        event.getEventUrl(), event.getImgUrl()))
+                .stream()
+                .toList();
     }
 
     private String uploadImg(MultipartFile multipartFile) {
